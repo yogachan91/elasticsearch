bd2b6598333fcf3e89f69cb0c333bd3bc72b9a3e
Yoga Chandra Widjaya <yogachandraw@gmail.com>
2025-12-05 07:43:53 +0700

    update

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ origins = [
     # Izinkan frontend lokal Anda
     "http://localhost",
     "http://localhost:8080",
-    "http://103.150.227.205:3000/",
+    "http://103.150.227.205:3000",
     # Jika ada, tambahkan domain produksi frontend Anda di masa depan
     # "https://domain-frontend-anda.com", 
 ]
